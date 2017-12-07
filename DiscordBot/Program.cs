e3ad609837e689830cfc4fe004f62e7bc0f48814
bsawhis <skyamachine@gmail.com>
2017-12-07 15:07:07 +0100

    Add files via upload

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace DiscordBot
 {
+    /// <summary>
+    /// Classe de threading
+    /// </summary>
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
-        {
+        { 
+            Console.Title = "Discord Youtube Bot";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            CommandReading:
+            string command = Console.ReadLine();
+
+            if(command == "help")
+            {
+                Console.WriteLine("help: affiche cet écran d'aide" + Environment.NewLine + "gnu: affiche la licence GNU de ce logiciel" + Environment.NewLine + "listen: met en écoute les actions d'upload de façon permanente");
+                goto CommandReading;
+            }
+            else if(command == "gnu")
+            {
+                Process.Start("http://www.gnu.org/licenses/gpl.html");
+                goto CommandReading;
+            }
+            else if(command == "listen")
+            {
+                goto CommandReading;
+            }
+            else
+            {
+                Console.WriteLine("Commande invalide. Essayez 'help'!");
+                goto CommandReading;
+            }
         }
     }
 }
